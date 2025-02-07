2ed1d341b1e56f6de10dc172e25d6668207d3217
Kerdonov <jalukse.kert@gmail.com>
2025-02-07 15:57:28 +0200

    added .gitignore, updated config

--- nvf-configuration.nix
+++ nvf-configuration.nix
@@ -16,5 +16,25 @@
       python.enable = true;
       html.enable = true;
     };
+    telescope.enable = true;
+    mini.statusline.enable = true;
+    autocomplete.nvim-cmp.enable = true;
+    autopairs.nvim-autopairs.enable = true;
+    keymaps = [
+      {
+        key = "<leader>ff";
+        mode = "n";
+        silent = true;
+        action = "require('telescope.builtin').find_files";
+        lua = true;
+      }
+      {
+        key = "<leader>fg";
+        mode = "n";
+        silent = true;
+        action = "require('telescope.builtin').live_grep";
+        lua = true;
+      }
+    ];
   };
 }
